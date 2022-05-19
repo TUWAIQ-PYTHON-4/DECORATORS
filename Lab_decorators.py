6d928b4bd17fef7eb369da5949ba93f45beebbf7
NOURAH <nourahsaadan@gmail.com>
2022-05-19 14:10:32 +0300

    Updated the py file

--- Lab_decorators.py
+++ Lab_decorators.py
@@ -5,21 +5,35 @@
 # This decorator should check that the argument is of type str and the length of the str is more than 5 , 
 # else raise an exception of type ValueError telling the user the argument should be str and it should have more than 5 characters .
 
-def check_argue_decorator(func):
-    def wrapper(*args):
-        if args is str and len(args) > 5:
-            print('Its a string with more than 5 chars')
+# decorator 
+def check_str(func):
+    # wrapper
+    def inner(*args):
+        print('inside wrapper before excuting')
+        func(*args)
+        print(args)
+        print(len(args))
+        if isinstance(args, str) and len(args) > 5:
+            print('String with more than 5 chars')
         else:
-            raise Exception("You should enter string more than 5 chars")
-        return func(*args)
-    return wrapper
+            raise ValueError("You should enter string more than 5 chars")
+        print('inside wrapper after excuting')
+    return inner
 
-@check_argue_decorator
-def func(*text):
-    a = text
-    return a
+# The Actual function
+def my_func(txt):
+    print(len(txt))
+    print('inside actual function')
+    
+    
+# passing 'my_func' inside the
+# decorator to control its behaviour
+my_func = check_str(my_func)
+ 
+ 
+# calling the function
+my_func('Hello World')
 
-user_input = input('Enter a string with more than 5 chars ...')
 
-func(user_input)
-
+### didn't work exactly
+## in the wrapper (inner) - the arge len = 1 ! couldn't fix it !
