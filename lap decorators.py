1d8dcd3a5fc96035d7550795bab22f2aa7308d7f
lama <l4m4100@gmail.com>
2022-05-19 14:28:08 +0300

    edit the solution

--- lap decorators.py
+++ lap decorators.py
@@ -1,11 +1,16 @@
 def document_it(func):
     def wrapper(*args):
-        if type(args) != str or len(args) < 5 :
-            try:
-                func(*args)
-            except:
-                print('The argument should be str and it should have more than 5 characters')
-        else:
+        if type(*args) == str and len(*args) > 5:
             return func(*args)
-            
+        else:
+            raise ValueError('The argument should be str and it should have more than 5 characters')
+
     return wrapper
+
+
+@document_it
+def unser_input(user_var: str):
+    print(user_var)
+
+
+unser_input("hi there")
