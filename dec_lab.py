c6973fbeb8e1ddaaa63440e2c3978d1248a1b37b
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-19 14:24:55 +0300

    Update

--- dec_lab.py
+++ dec_lab.py
@@ -1,5 +1,5 @@
-from msilib import type_string
 
+'''from msilib import type_string
 
 def decorator_function(func):
     def wrapper(*args, **kwargs):
@@ -15,4 +15,25 @@ def str_func(element:str):
     print(element)
     raise ValueError("Error")
 
-decorator_function('hehe')
+decorator_function('hehe')'''
+
+
+def str_check(fun):
+    def wrapper(*args,**kwargs):
+        for element in args:
+            if not isinstance(element,str) or not len(element)<=5:
+               raise ValueError("The parameter should be of type string and more than 5 ")
+        return fun(*args,**kwargs)
+    return wrapper
+
+def add_comment(comment:str):
+    print("Add your comment")
+
+def register_user(user_name,password):
+    print("Checking")
+comment_input=input("Enter your comment")  
+try:
+    add_comment(comment=comment_input)
+except Exception as e:
+    print(e)
+
