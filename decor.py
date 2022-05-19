45cc929d0070e81c476d1da25d85b55fad0b0ec3
RubaALmohya <ruba12399@gmail.com>
2022-05-19 13:58:01 +0300

    modified

--- decor.py
+++ decor.py
@@ -3,10 +3,11 @@ def check(func):
     def wrapper(*args):
     
         if type(*args) !=  str and len(*args) <5:
-            return func(*args)
-        else:   
             raise ValueError("the argument should be str and it should have more than 5 characters ")
             
+            
+        else:   
+            return func(*args)
     return wrapper
 
 
