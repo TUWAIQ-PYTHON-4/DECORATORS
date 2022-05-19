c807f5246a2dc276eb7dc476373a3a515a4697ce
RubaALmohya <ruba12399@gmail.com>
2022-05-19 14:13:17 +0300

    solved

--- dec2.py
+++ dec2.py
@@ -1,22 +1,19 @@
-
 def check(func):
     def wrapper(*args):
-    
-        if type(*args) !=  str and len(*args) <5:
+
+        if type(*args) !=  str or len(*args) <5:
             raise ValueError("the argument should be str and it should have more than 5 characters ")
-            
-            
+
+
         else:   
             return func(*args)
     return wrapper
-
-
 
 
 @check
 def user_input(str_user:str):
     print(" your string :" , str_user)
 try:
-    user_input("hi i am ruba")
+    user_input("hello")
 except Exception as e :
     print(e)
