7fc8c71a0b36755238c31ca485f763289696ec01
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-19 14:02:39 +0300

    Update

--- dec_lab.py
+++ dec_lab.py
@@ -3,21 +3,16 @@ from msilib import type_string
 
 def decorator_function(func):
     def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        return result
+        try : 
+          func(*args, **kwargs)
+          if type_string() != decorator_function & len(decorator_function) <= 5:
+           print("")
+        except (ValueError,TypeError):
+          print("invalid or your string is less than 5")    
     return wrapper
-
 
 def str_func(element:str):
     print(element)
     raise ValueError("Error")
-
-try :
-  decorator_function('heh')
-  if type_string() != decorator_function & len(decorator_function) <= 5:
-    print("")
-except (ValueError,TypeError):
-    print("invalid or your string is less than 5")
-
 
 decorator_function('hehe')
